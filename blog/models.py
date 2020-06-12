f227a7d22fbf23346bc3472631069599fbe87a18
Laura Cope <laura.kitty.cope@gmail.com>
2020-06-12 11:35:08 +0100

    Changed HTML

--- blog/models.py
+++ blog/models.py
@@ -2,9 +2,9 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-
+#Defines model - as on object called Post 
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)#link to another model
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
