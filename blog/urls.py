1c91f28fd38ba8c00cf8e063d14d91fd6624c197
Laura Cope <laura.kitty.cope@gmail.com>
2020-08-24 20:26:05 +0100

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from . import views
 """Assigning view post_list to root URL"""
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
