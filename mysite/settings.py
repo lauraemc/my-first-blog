d83f9deade0769187a357b6fcb5cb47aff605d39
Laura Cope <laura.kitty.cope@gmail.com>
2020-08-31 00:11:27 +0100

    static changed

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,7 +122,9 @@ USE_TZ = True
 #STATIC_URL = '/static/'
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+#STATIC_ROOT = ''
 
 STATIC_URL = '/static/'
 
