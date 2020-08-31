dca17a73cfcb914b4afeed38eaefee0a52a09819
Laura Cope <laura.kitty.cope@gmail.com>
2020-08-31 11:01:07 +0100

    static changed

--- mysite/settings.py
+++ mysite/settings.py
@@ -126,6 +126,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [ "/../../django_env/blog/static", ]
+#STATICFILES_DIRS = [ "/../../django_env/blog/static", ]
 
 
